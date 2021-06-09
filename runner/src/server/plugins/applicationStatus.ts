6571fa3d78ba777fd4be44d89c2a5e9698e7278f
jenbutongit <jen+git@cautionyourblast.com>
2021-06-09 16:40:29 +0100

    fix issue with retrying payments

--- runner/src/server/plugins/applicationStatus.ts
+++ runner/src/server/plugins/applicationStatus.ts
@@ -72,6 +72,7 @@ const applicationStatus = {
             if (payState.state.status !== "success" && !userCouldntPay) {
               return h.view("pay-error", {
                 reference,
+                visit: params.visit,
                 errorList: ["there was a problem with your payment"],
               });
             }
